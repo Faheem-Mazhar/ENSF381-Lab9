09975cbf36eed6e5242eaf912ff24eb8163868c2
abdulmoizbhatti <abdulmoiz.bhatti@ucalgary.ca>
2025-03-28 20:39:45 -0600

    finish prediction backend

--- house-price-predictor-frontend/src/components/Houseprediction.js
+++ house-price-predictor-frontend/src/components/Houseprediction.js
@@ -30,7 +30,7 @@ function HousePrediction() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/predict_house_price', {
+      const response = await fetch('http://127.0.0.1:5000/predict_house_price', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
